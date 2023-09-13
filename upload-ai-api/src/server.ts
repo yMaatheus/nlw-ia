f6ac53f8579da8baedf7378c9d1cdf2fe1bb7583
Matheus <ymatheus.h@gmail.com>
2023-09-13 11:49:23 -0300

    feat: :sparkles: setup CORS

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -3,8 +3,13 @@ import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAICompletationRoute } from "./routes/generate-ai-completion";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
