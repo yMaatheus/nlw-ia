2a1b6f8afb1d919447cfeaf737f7d23af764b48a
Matheus <ymatheus.h@gmail.com>
2023-09-13 11:27:57 -0300

    feat: :sparkles: create upload video route

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,9 +1,12 @@
 import { fastify  } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
 app.listen({
    port: 3333
