3ee885067610042516aa2405962532b424ff1444
Matheus <ymatheus.h@gmail.com>
2023-09-13 11:48:03 -0300

    feat: :sparkles: create generate ai route

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -2,12 +2,14 @@ import { fastify  } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletationRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAICompletationRoute);
 
 app.listen({
    port: 3333
